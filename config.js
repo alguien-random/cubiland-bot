f5c243039b0527ac262491ff1528a9ad49556304
alguien_random <soypro681@gmail.com>
2021-08-18 22:32:22 +0200

    bot

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.TOKEN = 'NzUzMjY5MDU4OTcxMTA3NDQ5.X1jueg.skx1wLvEgTDmPWB2osafem9Gljk';
+
+exports.PREFIX = '¿';
+
+exports.news_API = 'f95ae286e2cc41cdbd5f698fcb5f92d9';
+
+exports.giphy_API = 'U5WOaBksNctgixrWY2t0ZH4ugW0Na8oV';
+
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
